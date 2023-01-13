bb65f0801886e01de65bfb881318d9915d5e9c75
yukinishigaki99 <yukinishigaki@byui.edu>
2023-01-13 09:46:35 +0900

    fixed server.js

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const port = process.env.PORT || 3000;
 app.use('/', require('./routes'));
 
 app.listen(3000,() => {
-    console.log(`Server is running on ${3000}`);
+    console.log(`Server is running on ${port}`);
 });
