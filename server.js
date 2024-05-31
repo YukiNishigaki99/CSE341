29f4fcf043e34ceda5b2e3501155a0c984f53a7d
Yuki Nishigaki <yukinishigaki@byui.edu>
2024-05-31 22:35:36 +0900

    Week 6 team

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
+const port = process.env.PORT || 8080;
 const app = express();
 
-const port = 3000;
+app
+    .use(bodyParser.json())
+    .use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        next();
+    })
+    .use('/', require('./routes'));
 
-app.use('/', require('./routes'));
+process.on('uncaughtException', (err, origin) => {
+    console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
+});
 
-app.listen(process.env.port || port);
-console.log('Web Server is listening at port ' + (process.env.port || port));
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port);
+        console.log(`Connected to DB and listening on ${port}`);
+    }
+});
